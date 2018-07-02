57b00ac460522d436244eba257611638b45f09e2
kennethwty <kennethwty93@gmail.com>
2018-07-02 17:23:24 -0400

    Added Mergesort

--- src/Sorting_Algorithms/MergeSort.java
+++ src/Sorting_Algorithms/MergeSort.java
@@ -1,10 +1,16 @@
 package Sorting_Algorithms;
 
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
         int[] intArray = {6, 9, -1, 10, 10, 19, 2, 1, 17};
 
         mergesort(intArray, 0, intArray.length);
+
+        // Print out the result
+        System.out.print("Sorted Array: ");
+        System.out.print(Arrays.toString(intArray));
     }
 
     /**
@@ -29,6 +35,24 @@ public class MergeSort {
     }
 
     public static void merge(int[] arr, int start, int mid, int end) {
+        // if the last element on the left sub-array is smaller than the 1st element of the right sub-array
+        // then it is sorted
+        if(arr[mid - 1] <= arr[mid])
+            return;
 
+        int i = start; // first element of the left sub-array
+        int j = mid; // first element of the right sub-array
+        int index = 0; // to keep track of the index on the temp array
+        int[] temp = new int[end - start];
+
+        while(i < mid && j < end) {
+            // comparing the two sub-arrays to find the smaller element and it gets copied to the temp array
+            temp[index++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+        }
+
+        // if there are leftovers in the right sub-array, do nothing
+        // if there are leftovers in the left sub-array, copy them to the temp array
+        System.arraycopy(arr, i, arr, start + index, mid - i);
+        System.arraycopy(temp, 0, arr, start, index);
     }
 }
