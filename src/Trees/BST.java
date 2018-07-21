f748382cfe2c46df1cd55ea7109a2dea3325dbbe
kennethwty <kennethwty93@gmail.com>
2018-07-21 19:57:40 -0400

    Added Test Cases

--- src/Trees/BST.java
+++ src/Trees/BST.java
@@ -10,4 +10,19 @@ public class BST {
             root.insert(data);
         }
     }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+        tree.insert(35);
+        tree.insert(25);
+        tree.insert(15);
+        tree.insert(55);
+        tree.insert(85);
+        tree.insert(75);
+        tree.insert(95);
+        tree.insert(5);
+        tree.insert(65);
+        tree.insert(65);
+        tree.insert(105);
+    }
 }
