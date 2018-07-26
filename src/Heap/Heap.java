d24253607c83364b86d63ea2dd5f0dfacf4cedf2
kennethwty <kennethwty93@gmail.com>
2018-07-26 01:43:32 -0400

    Add peak() to heap structure

--- src/Heap/Heap.java
+++ src/Heap/Heap.java
@@ -15,7 +15,7 @@ public class Heap {
     // insert a new node to the end, and then heapify the structure above
     public void insert(int input) {
         // first check if the heap is full
-        if(isFull()) {
+        if (isFull()) {
             throw new IndexOutOfBoundsException("The heap capacity is full");
         }
         // insert to the end
@@ -26,7 +26,7 @@ public class Heap {
 
     // replace the deleted value with the rightmost node in the last tree level, then heapify
     public int delete(int targetIndex) {
-        if(isEmpty()) {
+        if (isEmpty()) {
             throw new IndexOutOfBoundsException("The heap is empty");
         }
         int parentIndex = getParentIndex(targetIndex);
@@ -34,7 +34,7 @@ public class Heap {
         // replace with the rightmost element
         heap[targetIndex] = heap[size - 1];
         // if it is the root node that we are deleting or its parent node is greater, then heapify below
-        if(targetIndex == 0 || heap[targetIndex] < heap[parentIndex]) {
+        if (targetIndex == 0 || heap[targetIndex] < heap[parentIndex]) {
             heapifyBelow(targetIndex, size - 1);
         } else {
             // heapify above
@@ -50,7 +50,7 @@ public class Heap {
         // store the new node value to a temp variable
         int temp = heap[index];
         // while it is not the root and while the child value is great than its parent value
-        while(index > 0 && temp > heap[getParentIndex(index)]) {
+        while (index > 0 && temp > heap[getParentIndex(index)]) {
             // copy the parent value first, swap at the very end after it located a spot
             heap[index] = heap[getParentIndex(index)];
             index = getParentIndex(index);
@@ -63,19 +63,19 @@ public class Heap {
     private void heapifyBelow(int parentIndex, int lastHeapIndex) {
         int swapWithChild;
 
-        while(parentIndex <= lastHeapIndex) {
+        while (parentIndex <= lastHeapIndex) {
             int leftChild = getChildIndex(parentIndex, true);
             int rightChild = getChildIndex(parentIndex, false);
-            if(leftChild <= lastHeapIndex) {
+            if (leftChild <= lastHeapIndex) {
                 // no right child, just left child
-                if(rightChild > lastHeapIndex) {
+                if (rightChild > lastHeapIndex) {
                     swapWithChild = leftChild;
                 } else {
                     // find the largest element to swap
                     swapWithChild = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
                 }
                 // swap with the largest of the two children nodes
-                if(heap[parentIndex] < heap[swapWithChild]) {
+                if (heap[parentIndex] < heap[swapWithChild]) {
                     int temp = heap[parentIndex];
                     heap[parentIndex] = heap[swapWithChild];
                     heap[swapWithChild] = temp;
@@ -98,6 +98,15 @@ public class Heap {
 
     public int getChildIndex(int parentIndex, boolean left) {
         return 2 * parentIndex + (left ? 1 : 2);
+    }
+
+    // peek method checks what's in the root
+    public int peek() {
+        if(isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        return heap[0];
     }
 
     // checks whether the heap is full
@@ -136,5 +145,8 @@ public class Heap {
 
         heap.delete(1);
         heap.printHeap();
+
+        System.out.println();
+        System.out.print("Root Node: " + heap.peek());
     }
 }
